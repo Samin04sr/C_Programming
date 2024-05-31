2f5172b65d93f06da0444d4951d14ac7f0da2ca1
Samin04sr <saminrashid43@gmail.com>
2024-05-31 16:52:42 +0600

    test file updated

--- test.c
+++ test.c
@@ -5,9 +5,9 @@ int main()
     int a; // 4 bytes (by-default long)
     long int a1; // 4 bytes
     short int a2; // 2 bytes
-    float b; //
-    double c;
-    char d;
+    float b; // 4 byte
+    double c; // 8 byte
+    char d; // 1 byte
     printf("%d %d %d %d",sizeof(a),sizeof(b),sizeof(c),sizeof(d));
     return 0;
 }
